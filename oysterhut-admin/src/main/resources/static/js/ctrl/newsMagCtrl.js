8d561fb7c19957181997da8b73c839112042d621
gjye <gangjie.ye@joinwe.com.cn>
2018-03-13 17:25:53 +0800

    牡蛎小屋项目--newsMag.html

---
+++ oysterhut-admin/src/main/resources/static/js/ctrl/newsMagCtrl.js
@@ -0,0 +1,30 @@
+$(function () {
+    $("#logoutA").click(function () {
+        $.get("/logout", {}, function (result) {
+            if (result.status == 200) {
+                location.href = "/login.html";
+            } else {
+                $("#erro").html(result.message);
+            }
+        });
+    });
+});
+//上传图片显示缩略图
+function previewImage(obj) {
+    var imgFile = obj;
+    var path;
+    if (document.all) {//IE浏览器
+        imgFile.select();
+        path = document.selection.createRange().text;
+        var previewImageDivObj = document.getElementById("preview_image_div");
+        previewImageDivObj.innerHTML = "";
+        previewImageDivObj.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled='true',sizingMethod='scale',src=\""
+            + path + "\")";// 使用滤镜效果
+        previewImageDivObj.style.display = "block";
+    } else {
+        path = URL.createObjectURL(imgFile.files[0]);
+        $("#preview_image_div").html("");
+        $("#preview_image_div").html("<img id='news_img_url_preview' alt='' src='" + path + "' style='width: 220px;height: 100px;'/>");
+        $("#preview_image_div").show();
+    }
+}
